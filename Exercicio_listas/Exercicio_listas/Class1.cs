75d10f8e5f28e1f3342963bb2bba83c69613a619
mtsgo13 <marheuseric@gmail.com>
2022-09-05 10:30:52 -0300

    Exercicio

---
+++ Exercicio_listas/Exercicio_listas/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_listas
+{
+    class Class1
+    {
+    }
+}
---
+++ Exercicio_listas/Exercicio_listas/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exercicio_listas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> list = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Emplyoee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int idVeri = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == idVeri);
+
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentge = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentge);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Updated list of employees: ");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+
+
+    }
+}
